dd8dc632802ae9b35b8744b906c812ed669d654f
open1234 <2506705328@qq.com>
2016-03-23 10:51:11 +0800

    codeg 项目初始化

---
+++ src/main/java/my/xujb/codeg/App.java
@@ -0,0 +1,12 @@
+package my.xujb.codeg;
+
+/**
+ * @author 徐静波
+ * @version 1.0
+ * @description codeg代码生成主入口
+ */
+public class App {
+	public static void main(String[] args) {
+		// TODO
+	}
+}
---
+++ src/main/java/my/xujb/codeg/generator/base/BaseGenerator.java
@@ -0,0 +1,10 @@
+package my.xujb.codeg.generator.base;
+
+/**
+ * @author 徐静波
+ * @version 1.0
+ * @description 代码生成器, 抽象父类
+ */
+public abstract class BaseGenerator {
+
+}
